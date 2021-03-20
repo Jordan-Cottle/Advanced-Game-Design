14fb3f2682b0fff627fffe900917ffe09e754d80
Jordan Cottle <jordancottle622@gmail.com>
2021-03-20 14:00:47 -0400

    Add acceleration down tunnel

--- Advanced-Game-Design/Assets/Scripts/MovementController.cs
+++ Advanced-Game-Design/Assets/Scripts/MovementController.cs
@@ -4,28 +4,21 @@ public class MovementController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float MovementSpeed = 15f;
-    public float Gravity = -10f;
+    public float Acceleration = 1f;
     public CharacterController Controller;
-    public Transform GroundCheck;
-    public float GroundDistance;
-    public LayerMask GroundMask;
-
     bool grounded;
     Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal") * MovementSpeed;
+        float verticalInput = Input.GetAxis("Vertical") * MovementSpeed;
 
-        if (Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask))
-        {
-            velocity.y = 0;
-        }
+        velocity.x = -horizontalInput;
+        velocity.y = verticalInput;
+        velocity.z -= Acceleration * Time.deltaTime;
 
-        float horizontalInput = Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime;
-        float verticalInput = Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime;
-
-
-        Controller.Move(new Vector3(-horizontalInput, verticalInput, 0));
+        Controller.Move(velocity * Time.deltaTime);
     }
 }
