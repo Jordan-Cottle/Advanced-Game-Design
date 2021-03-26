d95bd25cd7941214bb6caffd988bede6b893aa12
Jordan Cottle <jordancottle622@gmail.com>
2021-03-25 21:59:25 -0400

    Don't reset obstacle position when it leaves scene

--- Advanced-Game-Design/Assets/Scripts/Obstacle.cs
+++ Advanced-Game-Design/Assets/Scripts/Obstacle.cs
@@ -5,27 +5,13 @@ public class Obstacle : MonoBehaviour
     public int Durability = 5;
     public bool Active = true;
 
+    private Vector3 gone = new Vector3(-100, -100, 0);
+
     void Update()
     {
         if (this.transform.position.z < -5)
         {
-            float x;
-            float y;
-            float z;
-            if (this.Active)
-            {
-                x = Random.Range(-10f, 10f);
-                y = Random.Range(-10f, 10f);
-                z = Random.Range(25f, 50f);
-            }
-            else
-            {
-                x = -100;
-                y = -100;
-                z = 0;
-            }
-
-            this.transform.position = new Vector3(x, y, z);
+            this.Remove();
         }
     }
     void OnCollisionEnter(Collision collision)
@@ -34,8 +20,13 @@ public class Obstacle : MonoBehaviour
         if (Durability <= 0)
         {
             Debug.Log("Obstacle destroyed!");
-            this.transform.position = new Vector3(-100, -100, 0);
-            this.Active = false;
+            this.Remove();
         }
+    }
+
+    void Remove()
+    {
+        this.transform.position = gone;
+        this.Active = false;
     }
 }
