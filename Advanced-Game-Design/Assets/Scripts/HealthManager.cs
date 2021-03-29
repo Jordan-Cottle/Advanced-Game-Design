529b3c3a82a47d69ae6f72ca9fb3fa479614a8b5
Jordan Cottle <jordancottle622@gmail.com>
2021-03-29 15:51:05 -0400

    Add HealthManager for tracking player health

---
+++ Advanced-Game-Design/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+
+    public float MaxHealth;
+    public Slider HealthBar;
+
+
+    private float _currentHealth;
+    public float CurrentHealth
+    {
+        get => _currentHealth; private set
+        {
+            _currentHealth = Mathf.Max(value, 0);
+            HealthBar.value = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBar.minValue = 0;
+        HealthBar.maxValue = MaxHealth;
+
+        CurrentHealth = MaxHealth;
+    }
+}
