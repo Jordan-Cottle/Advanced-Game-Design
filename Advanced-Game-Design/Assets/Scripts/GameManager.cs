8d70203cb3fa091b6fd6fccd3ed667ad15af2f1d
Jordan Cottle <jordancottle622@gmail.com>
2021-03-29 15:12:00 -0400

    Update speed in UI

--- Advanced-Game-Design/Assets/Scripts/GameManager.cs
+++ Advanced-Game-Design/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class GameManager : MonoBehaviour
     private Queue<Obstacle> availableObstacles = new Queue<Obstacle>();
     private List<Obstacle> obstacles = new List<Obstacle>();
     public List<GameObject> TunnelPieces;
+    public Text SpeedLabel;
 
     private Vector3 tunnelJump;
     private float tunnelScale;
@@ -48,6 +50,7 @@ public class GameManager : MonoBehaviour
         }
 
         velocity.z = Mathf.Clamp(velocity.z - Acceleration * Time.deltaTime, -MaxSpeed, 0f);
+        SpeedLabel.text = $"Speed: {CurrentSpeed}";
 
         foreach (var obstacle in obstacles)
         {
